5d7de66d711b9db29f4a41ff96ebb14f38f01961
TBrusilovsky <55800776+TBrusilovsky@users.noreply.github.com>
2021-04-21 22:14:24 -0400

    testing files and minor code tweaks. items cut

---
+++ docs/script.js
@@ -0,0 +1,26 @@
+
+
+
+function moveLeft(event) {
+    let mp4 = document.getElementById("mp4");
+    mp4.src= "Media/" + -filename- + ".mp4";
+
+    let webm = document.getElementById("webm");
+    mp4.src= "Media/" + -filename- + ".webm";
+
+    let ogv = document.getElementById("ogv/ogg");
+    mp4.src= "Media/" + -filename- + ".OGV";
+
+
+}
+function moveRight(event) {
+    let mp4 = document.getElementById("mp4");
+    mp4.src= "Media/" + -filename- + ".mp4";
+
+    let webm = document.getElementById("webm");
+    mp4.src= "Media/" + -filename- + ".webm";
+
+    let ogv = document.getElementById("ogv/ogg");
+    mp4.src= "Media/" + -filename- + ".OGV";
+
+}
