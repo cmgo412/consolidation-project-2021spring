51e2fc419d6e815eb874c150dcd9123b98565d68
Caela Go <cmg139@pitt.edu>
2021-04-28 17:27:36 -0400

    Linked correct videos

--- docs/script.js
+++ docs/script.js
@@ -1,20 +1,93 @@
 
-let current = {
-    curVal: "v1",
+let current = { //staying in room vs. leaving room 
+    curVal: "choice1_start",
     left: null,
     right: null
     }
 
-current.left = { 
-        curVal: "v2",
+current.left = { //staying in room: using phone vs. laptop
+        curVal: "choice2_phone_vs_laptop",
         left:null ,
         right: null
     }
-current.right = {
-        curVal: "v3",
+
+current.left.left = { //using laptop: playing video games vs. doing work
+    curVal: "choice2a_game_vs_work",
+    left: null,
+    right: null
+}
+
+current.left.left.left = { //bad ending: playing video games
+    curVal: "choice2a_video_games",
+    left: null,
+    right: null
+}
+
+current.left.left.right = { //bad ending: doing work
+    curVal: "choice2a_work",
+    left: null,
+    right: null
+}
+
+current.left.right = { //using phone: cute vs. scary content
+    curVal: "choice2b_cute_vs_scary",
+    left: null,
+    right: null
+}
+
+current.left.right.left = { //good ending: cute content
+    curVal: "choice2b_cute",
+    left: null,
+    right: null
+}
+
+current.left.right.right = { //bad ending: scary content
+    curVal: "choice2b_scary",
+    left: null,
+    right: null
+}
+
+current.right = { //leaving room: living room vs. kitchen
+        curVal: "choice3_livingroom_vs_kitchen",
         left: null ,
         right: null
     }
+
+current.right.left = { //kitchen: roommate's food vs. milk
+    curVal: "choice3a_milk_vs_food",
+    left: null,
+    right: null
+}
+
+current.right.left.left = { //bad ending: roommate's food
+    curVal: "choice3a_roommate_food",
+    left: null,
+    right: null
+}
+
+current.right.left.right = { //bad ending: spoiled milk
+    curVal: "choice3a_milk",
+    left: null,
+    right: null
+}
+
+current.right.right = { //living room: watching tv vs. sleeping on couch
+    curVal: "choice3b_couch_vs_tv",
+    left: null,
+    right: null
+}
+
+current.right.right.left = { //bad ending: watching tv
+    curVal: "choice3b_tv",
+    left: null,
+    right: null
+}
+
+current.right.right.right = { //bad ending: sleeping on couch
+    curVal: "choice3b_couch",
+    left: null,
+    right: null
+}
 
 function setup() {
 
@@ -43,13 +116,13 @@ function moveLeft(event) {
 
 
     let mp4 = document.getElementById("mp4");
-    mp4.src= "Media/" + current.curVal + ".mp4";
+    mp4.src= "Media/footage/" + current.curVal + ".mp4";
 
-    let webm = document.getElementById("webm");
-    webm.src= "Media/" + current.curVal + ".webm";
+    // let webm = document.getElementById("webm");
+    // webm.src= "Media/footage/" + current.curVal + ".webm";
 
-    let ogv = document.getElementById("ogv/ogg");
-    ogv.src= "Media/" + current.curVal + ".OGV";
+    // let ogv = document.getElementById("ogv/ogg");
+    // ogv.src= "Media/footage/" + current.curVal + ".OGV";
 
     let videoSourse = document.getElementById("video");
     videoSourse.load();
